2fd25507419e89867281a146feed556f5d5a6ff5
aguspre <80070229+aguspre@users.noreply.github.com>
2021-05-10 19:26:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use( express.json({ extended: true }));
 
 // Puerto de la app
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT  || 4001;
 
 // Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 // arrancar la app
-app.listen(PORT, () =>{
+app.listen(PORT, '0.0.0.0', () =>{
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
 
